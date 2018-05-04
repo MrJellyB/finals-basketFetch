5fc4668ddfdbd75b56b46580f58084ac0cecace8
MrJellyB <shvachernoam@gmail.com>
2018-05-04 18:44:16 +0300

    Created intrerface and base class for criteria attribute

---
+++ Basket.Common/Basket.Common/Attribute/Critieria.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Basket.Common.Attribute
+{
+    public abstract class BaseCritieria : System.Attribute, ICriteria
+    {
+
+        #region Data Members
+
+
+
+        #endregion
+
+        #region Methods
+
+        public abstract float Calculate();
+
+        #endregion
+    }
+}
---
+++ Basket.Common/Basket.Common/Attribute/ICriteria.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basket.Common.Attribute
+{
+    public interface ICriteria
+    {
+        #region Methods
+
+        float Calculate(); 
+
+        #endregion
+    }
+}
