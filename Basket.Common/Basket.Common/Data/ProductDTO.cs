a8c52b9fe1bc4d92d9491c368e650a082037dd3e
liormiz <liorstrongxd>
2018-05-05 17:51:32 +0300

    fixed the product dto

--- Basket.Common/Basket.Common/Data/ProductDTO.cs
+++ Basket.Common/Basket.Common/Data/ProductDTO.cs
@@ -31,8 +31,10 @@ namespace Basket.Common.Data
         
         [BsonRepresentation(BsonType.Double, AllowTruncation = true)]
         public float Quantity { get; set; }
-        
+
         public int bIsWeighted { get; set; }
+
+        public int calories { get; set; }
         
         public string UnitOfMeasure { get; set; }
         
@@ -49,5 +51,35 @@ namespace Basket.Common.Data
         public int ItemStatus { get; set; }
         
         public int category { get; set; }
+
+        [BsonElement]
+        public IList<OldPriceDTO> oldPriceArray { get; set; }
+
+        [BsonElement]
+        public IList<CommentsDTO> comments { get; set; }
+    }
+
+    public class OldPriceDTO
+    {
+        [BsonRepresentation(BsonType.Double, AllowTruncation = true)]
+        public double curr { get; set; }
+
+        [BsonRepresentation(BsonType.DateTime, AllowTruncation = true)]
+        public DateTime createdTime { get; set; }
+
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string _id { get; set; }
+    }
+
+    public class CommentsDTO
+    {
+        public string comment { get; set; }
+        
+        public int grade { get; set; }
+
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string _id { get; set; }
     }
 }
--- Basket.ServerSide/Program.cs
+++ Basket.ServerSide/Program.cs
@@ -11,8 +11,9 @@ namespace Basket.ServerSide
             ConnectionMongoDB db = new ConnectionMongoDB();
             db.InitMongoClient(ConnectionMongoDB.URL);
             List<GenderDTO> genders = db.GetAllGenders();
-            List<ProductDTO> products = db.GetAllProductDTO();
+            ProductDTO currProduct2 = db.GetProductDTOByProductId(25675120020);
             ProductDTO currProduct = db.GetProductDTOByProductId(34000196173);
+            List<ProductDTO> products = db.GetAllProductDTO();
             List<CategoryDTO> category = db.GetAllCategoriesDTO();
             List<BasketDTO> baskets = db.GetAllBasketsDTO();
         }
